3f08e64201d4eda4eb89c63f70aec083d4029c06
priyankarcoding <mpriyankar276@gmail.com>
2026-09-15 13:39:53 +0530

    it is a grocery app

---
+++ app/src/main/java/com/example/smp2kart/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.example.smp2kart;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Connect Java with XML
+        setContentView(R.layout.activity_login);
+
+        // Connect XML components
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        // Login button working
+        btnLogin.setOnClickListener(v -> {
+
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+
+            if (email.isEmpty()) {
+                etEmail.setError("Enter Email");
+                etEmail.requestFocus();
+                return;
+            }
+
+            if (password.isEmpty()) {
+                etPassword.setError("Enter Password");
+                etPassword.requestFocus();
+                return;
+            }
+
+            Toast.makeText(
+                    LoginActivity.this,
+                    "Login Successful",
+                    Toast.LENGTH_SHORT
+            ).show();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/smp2kart/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.smp2kart;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(
+                        SplashActivity.this,
+                        LoginActivity.class
+                );
+
+                startActivity(intent);
+            }
+        }, SPLASH_TIME);
+    }
+}
